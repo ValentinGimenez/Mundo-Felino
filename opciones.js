5704d7f39a845a7fea886953ac9dcbdb7e52b55b
Valentin Alejandro Gimenez <valentingimenez1909@gmail.com>
2023-04-26 16:30:47 -0300

    indent

--- opciones.js
+++ opciones.js
@@ -2,7 +2,7 @@ let opc = [
     {
         tipo: "FELINOS DOMESTICOS",
         animal: "GATO AZUL RUSO",
-        info: "El Azul Ruso es una raza de gato originaria de Rusia que se caracteriza por su pelaje corto, suave y de color gris azulado. Es un gato de tamaño mediano con una complexión delgada y elegante. Su cabeza es angular y tiene grandes orejas puntiagudas. El Azul Ruso es conocido por ser un gato muy cariñoso, inteligente y sociable. A menudo se vincula muy estrechamente con sus dueños y disfruta de su compañía.",
+        info: "El Azul Ruso es una raza de gato originaria de Rusia que se caracteriza por su pelaje corto, suave y de color gris azulado. Es un gato de tamaño mediano con una complexión delgada y elegante. Su cabeza es angular y tiene grandes orejas puntiagudas. El Azul Ruso es conocido por ser un gato muy cariñoso, inteligente y sociable. A menudo se vincula muy estrechamente con sus dueños y disfruta de su compañía. Es una raza de gato muy activa y le encanta jugar, especialmente con juguetes que estimulan sus instintos de caza.",
         imagen: 'img/animales/domesticos/azulruso.jpg'
     },
     {
@@ -32,19 +32,19 @@ let opc = [
     {
         tipo: "FELINOS DOMESTICOS",
         animal: "GATO SIAMES",
-        info: "El Siames es una raza de gato originaria de Tailandia que se caracteriza por su pelaje corto y suave de color crema o marrón claro con marcas distintivas de color oscuro en las extremidades, la cola, las orejas y el rostro. Es un gato de tamaño mediano con una complexión esbelta y elegante. Su cabeza es triangular y tiene grandes ojos almendrados de color azul claro. El Siames es conocido por ser un gato muy vocal, activo y curioso.",
+        info: "El Siames es una raza de gato originaria de Tailandia que se caracteriza por su pelaje corto y suave de color crema o marrón claro con marcas distintivas de color oscuro en las extremidades, la cola, las orejas y el rostro. Es un gato de tamaño mediano con una complexión esbelta y elegante. Su cabeza es triangular y tiene grandes ojos almendrados de color azul claro. El Siames es conocido por ser un gato muy vocal, activo y curioso. A menudo se vincula muy estrechamente con sus dueños y le encanta jugar y explorar su entorno. Es una raza de gato muy inteligente y sociable que disfruta de la compañía humana y la de otros animales domésticos. Es importante destacar que el Siames requiere de un cuidado adecuado de su pelaje, ya que su pelaje corto y suave puede enredarse fácilmente.",
         imagen: 'img/animales/domesticos/siames.jpg'
     },
     {
         tipo: "FELINOS SALVAJES",
         animal: "GUEPARDO",
-        info: "El guepardo es un felino de cuerpo delgado y musculoso, con una piel amarillenta o marrón claro cubierta de manchas negras. Sus patas largas y flexibles le permiten correr a velocidades extremadamente altas, lo que lo convierte en el animal terrestre más rápido del mundo. Su cabeza es pequeña y redondeada, con un hocico corto y orejas pequeñas y redondeadas. Los guepardos son animales solitarios y prefieren vivir en áreas abiertas, como sabanas y praderas.",
+        info: "El guepardo es un felino de cuerpo delgado y musculoso, con una piel amarillenta o marrón claro cubierta de manchas negras. Sus patas largas y flexibles le permiten correr a velocidades extremadamente altas, lo que lo convierte en el animal terrestre más rápido del mundo. Su cabeza es pequeña y redondeada, con un hocico corto y orejas pequeñas y redondeadas. Los guepardos son animales solitarios y prefieren vivir en áreas abiertas, como sabanas y praderas. Son cazadores diurnos y se alimentan principalmente de pequeños antílopes y otros animales pequeños.",
         imagen: 'img/animales/salvajes/guepardo.jpg'
     },
     {
         tipo: "FELINOS SALVAJES",
         animal: "LEON",
-        info: "El león es uno de los felinos más reconocidos en todo el mundo, conocido por su melena de color marrón oscuro alrededor del cuello. Los machos suelen ser más grandes y pesados que las hembras, y ambos sexos tienen un pelaje de color marrón dorado. Los leones viven en manadas lideradas por un macho alfa y se alimentan principalmente de presas grandes como cebras, búfalos y antílopes. Los leones tienen una cabeza grande y poderosa con dientes afilados y garras retráctiles.",
+        info: "El león es uno de los felinos más reconocidos en todo el mundo, conocido por su melena de color marrón oscuro alrededor del cuello. Los machos suelen ser más grandes y pesados que las hembras, y ambos sexos tienen un pelaje de color marrón dorado. Los leones viven en manadas lideradas por un macho alfa y se alimentan principalmente de presas grandes como cebras, búfalos y antílopes. Los leones tienen una cabeza grande y poderosa con dientes afilados y garras retráctiles. También tienen una buena vista y oído, lo que les ayuda a detectar presas y peligros potenciales.",
         imagen: 'img/animales/salvajes/leon.jpg'
     },
     {
@@ -56,31 +56,31 @@ let opc = [
     {
         tipo: "FELINOS SALVAJES",
         animal: "PUMA",
-        info: "También conocido como león de montaña o pantera, el puma es un felino de tamaño mediano a grande que se encuentra en América del Norte, América Central y América del Sur. Tienen una piel de color marrón dorado con manchas negras y una cabeza redondeada con orejas pequeñas y redondeadas. Son solitarios y crepusculares, lo que significa que son más activos al amanecer y al atardecer. Se alimentan de una variedad de animales, incluyendo venados, alces y roedores.",
+        info: "También conocido como león de montaña o pantera, el puma es un felino de tamaño mediano a grande que se encuentra en América del Norte, América Central y América del Sur. Tienen una piel de color marrón dorado con manchas negras y una cabeza redondeada con orejas pequeñas y redondeadas. Son solitarios y crepusculares, lo que significa que son más activos al amanecer y al atardecer. Se alimentan de una variedad de animales, incluyendo venados, alces y pequeños roedores. Los pumas son muy ágiles y pueden saltar hasta seis veces su propia longitud en un solo salto.",
         imagen: 'img/animales/salvajes/puma.jpg'
     },
     {
         tipo: "FELINOS EN PELIGRO DE EXTINCION",
         animal: "TIGRE DE BENGALA",
-        info: "El tigre de Bengala es uno de los felinos más majestuosos y fascinantes del mundo. Es uno de los mayores felinos y se caracteriza por sus rayas verticales negras y naranjas, que le ayudan a camuflarse en su entorno. Habita en los bosques y selvas de India, Bangladesh, Bhután y Nepal. Los machos pueden llegar a pesar hasta 250 kg, mientras que las hembras son más pequeñas. El tigre de Bengala es un depredador solitario y se alimenta principalmente de ungulados como ciervos, búfalos y jabalíes.",
+        info: "El tigre de Bengala es uno de los felinos más majestuosos y fascinantes del mundo. Es uno de los mayores felinos y se caracteriza por sus rayas verticales negras y naranjas, que le ayudan a camuflarse en su entorno. Habita en los bosques y selvas de India, Bangladesh, Bhután y Nepal. Los machos pueden llegar a pesar hasta 250 kg, mientras que las hembras son más pequeñas. El tigre de Bengala es un depredador solitario y se alimenta principalmente de ungulados como ciervos, búfalos y jabalíes. A pesar de que su población ha disminuido significativamente en las últimas décadas, se han tomado medidas de conservación para proteger a esta especie, y su número ha aumentado en algunas áreas.",
         imagen: 'img/animales/extincion/bengala.jpg'
     },
     {
         tipo: "FELINOS EN PELIGRO DE EXTINCION",
         animal: "GATO DE PALLAS",
-        info: "El gato de Pallas es un felino pequeño y rechoncho que habita en las estepas y desiertos de Asia central. Su pelaje es de color gris pálido con rayas negras y tiene un cuerpo robusto y piernas cortas. Es un cazador solitario y se alimenta principalmente de pequeños mamíferos, aves y reptiles. Se mueve principalmente al amanecer y al atardecer para evitar el calor del día. Es un animal tímido y esquivo que rara vez se deja ver por los seres humanos.",
+        info: "El gato de Pallas es un felino pequeño y rechoncho que habita en las estepas y desiertos de Asia central. Su pelaje es de color gris pálido con rayas negras y tiene un cuerpo robusto y piernas cortas. Es un cazador solitario y se alimenta principalmente de pequeños mamíferos, aves y reptiles. Se mueve principalmente al amanecer y al atardecer para evitar el calor del día. Es un animal tímido y esquivo que rara vez se deja ver por los seres humanos. A pesar de que no se encuentra en peligro crítico de extinción, la población del gato de Pallas se ha reducido en algunas áreas debido a la caza furtiva y la degradación del hábitat.",
         imagen: 'img/animales/extincion/gatopallas.jpg'
     },
     {
         tipo: "FELINOS EN PELIGRO DE EXTINCION",
         animal: "LEOPARDO DE LAS NIEVES",
-        info: "El leopardo de las nieves es un felino hermoso y misterioso que habita en las montañas de Asia central. Su pelaje es de color gris plateado con manchas negras, lo que le ayuda a camuflarse en su entorno rocoso. El leopardo de las nieves es un cazador solitario y se alimenta principalmente de cabras, ovejas y otros ungulados de montaña. Es un animal ágil y poderoso que puede saltar hasta seis veces su propia longitud.",
+        info: "El leopardo de las nieves es un felino hermoso y misterioso que habita en las montañas de Asia central. Su pelaje es de color gris plateado con manchas negras, lo que le ayuda a camuflarse en su entorno rocoso. El leopardo de las nieves es un cazador solitario y se alimenta principalmente de cabras, ovejas y otros ungulados de montaña. Es un animal ágil y poderoso que puede saltar hasta seis veces su propia longitud. La población de leopardo de las nieves se ha reducido drásticamente debido a la caza furtiva y la pérdida de hábitat. Actualmente, se considera una especie en peligro de extinción.",
         imagen: 'img/animales/extincion/leopardonieve.jpg'
     },
     {
         tipo: "FELINOS EN PELIGRO DE EXTINCION",
         animal: "LINCE BOREAL",
-        info: "El lince boreal es un felino que vive en las regiones boscosas del norte de Europa y Asia. Es un animal hermoso y elegante que tiene un pelaje denso y suave de color marrón claro con manchas negras. Los linces boreales son cazadores solitarios y se alimentan principalmente de conejos, liebres y otros pequeños mamíferos. Son animales nocturnos y tímidos que prefieren evitar a los humanos.",
+        info: "El lince boreal es un felino que vive en las regiones boscosas del norte de Europa y Asia. Es un animal hermoso y elegante que tiene un pelaje denso y suave de color marrón claro con manchas negras. Los linces boreales son cazadores solitarios y se alimentan principalmente de conejos, liebres y otros pequeños mamíferos. Son animales nocturnos y tímidos que prefieren evitar a los humanos. La población de lince boreal se ha reducido significativamente debido a la caza y la degradación del hábitat, y actualmente se considera una especie en peligro de extinción en muchas regiones donde habita. A pesar de los esfuerzos de conservación, la población de lince boreal sigue disminuyendo en algunas áreas debido a la pérdida de hábitat y la caza furtiva. Es importante seguir trabajando en medidas de protección y conservación para garantizar la supervivencia de esta especie en el futuro.",
         imagen: 'img/animales/extincion/linceboreal.jpg'
     }
 ]
